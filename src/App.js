f2e72f1cd074b7df2274e7c1ced8533aea8ce4d0
Muhammad Ali <engr.alikhan2001@gmail.com>
2024-05-28 17:17:00 +0500

    map

--- src/App.js
+++ src/App.js
@@ -3,22 +3,33 @@ import './App.css';
 
 function App() {
   return (
+    <div className="Ap">
+      
+      <h1>
+          Vehicle Location display 
+      </h1>
+      
+
     <div className="App">
+
       <header className="App-header">
+
+
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Assalamoalikom Ali, edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/MuhammadAli1132001"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn more about my working experience
         </a>
       </header>
-    </div>
+    </div>    </div>
+
   );
 }
 
