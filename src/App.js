8cf90d0b2f67b5efa460d3ae76bb1a3e5d6e6369
Muhammad Ali <engr.alikhan2001@gmail.com>
2024-05-29 16:39:08 +0500

    web app

--- src/App.js
+++ src/App.js
@@ -2,18 +2,38 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+//   // Filename - App.js
+// const element = (
+// 	<div>
+// 		<h1>This is Heading 1 </h1>
+// 		<h2>This is Heading 2</h2>
+// 		<h3>This is Heading 3 </h3>
+// 	</div>
+// );
+
+// ReactDOM.render(element, document.getElementById("root"));
+
+
   return (
     <div className="Ap">
-      
+      <div className="App">
+
+      <header className="App-header">  
       <h1>
-          Vehicle Location display 
+        Vehicle Location display 
       </h1>
+     	<h1>This is Heading 1 </h1>
+   		<h2>This is Heading 2</h2>
+		  <h3>This is Heading 3 </h3>
+    <table>
+      <tr>
+      <td><button id = "control-button-stop"> stop</button></td>
+      <td><button id = "control-button-run"> Run</button></td>
+      </tr>
+    </table>
       
-
-    <div className="App">
-
-      <header className="App-header">
-
+ 
 
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -25,10 +45,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn more about my working experience
+        Learn more about my working experience
         </a>
       </header>
-    </div>    </div>
+    </div>    
+  </div>
 
   );
 }
