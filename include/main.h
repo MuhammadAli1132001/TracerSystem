7e8b61ca9327d5ea11929870008e9b6594a76c2f
Muhammad Ali <engr.alikhan2001@gmail.com>
2024-08-05 14:50:08 +0500

    push

--- include/main.h
+++ include/main.h
@@ -1,20 +1,14 @@
-#ifndef Main_h
-#define Main_h
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <Arduino.h>
 #include <WebServer.h>
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 #include <LittleFS.h>
-#include "time.h"
-#include "firebase.h"
-// #if defined(ESP32)
 #include <WiFi.h>
-// #elif defined(ESP8266)
-// #include <ESP8266WiFi.h>
-// #endif
 
-// Insert your network credentials
+
 #define WIFI_SSID "CST WIFI"
 #define WIFI_PASSWORD "iotdevs123"
 
@@ -22,10 +16,15 @@
 #define WifiConnectedLed 7
 #define Dht_Sensor_Pin 12
 #define Dht_type DHT11
+#define switch_pin 3
+#define database_led 5
 
 void File_handle(void);
 void WiFi_setup(void);
+// void check_button(void);
+void firebase_setup(void);
 void temperature_humidity_firebase(float Temperature, float Humidity);
 void current_voltage_capacity_to_firebase(void);
-void check_button(void);
+void check_button_status(void);
+
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,23 @@
 #include "main.h"
-#include "googlesheetstorage.h"
+#include <Firebase_ESP_Client.h>
 
-ESP_Google_Sheet_Client Gosheet;
+#include "addons/TokenHelper.h"
+#include "addons/RTDBHelper.h"
+
+#define API_KEY "AIzaSyCc9UlEu4d95dMJ85aWfMsxHEybkH7CUtQ"
+#define DATABASE_URL "https://tracer-system-with-esp32-default-rtdb.asia-southeast1.firebasedatabase.app/"
+
+FirebaseData fbdo;
+FirebaseAuth auth;
+FirebaseConfig config;
+
+bool signupOK = false;
+uint8_t capacity = 90;
+uint8_t current = 20;
+uint8_t voltage = 63;
+uint8_t Kmrange = 140;
+float_t Temp, Humi;
+uint8_t led_status;
 
 // DHT sensor
 DHT dht_sensor(Dht_Sensor_Pin, Dht_type);
@@ -12,28 +28,23 @@ float temp;
 float humi;
 float pres;
 float longitude, altitude;
-unsigned long epochTime;
-
-const char* ntpServer = "pool.ntp.org";
+unsigned long sendDataPrevMillis = 0;
 
 void setup() {
     Serial.begin(115200);
     pinMode(DataSendedLed, OUTPUT);
     pinMode(switch_pin, INPUT);
     pinMode(database_led, OUTPUT);
-
-    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-    Serial.print("Connecting to Wi-Fi");
     
     WiFi_setup();
     File_handle();
-    initializeGSheet();
+    firebase_setup();
     dht_sensor.begin();
 }
 
 void loop() {
 
-    check_button();
+    // check_button();
 
     if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 5000 || sendDataPrevMillis == 0)) {
         sendDataPrevMillis = millis();
@@ -46,20 +57,6 @@ void loop() {
         current_voltage_capacity_to_firebase();
         check_button_status();
 
-    }
-
-    bool ready = Gosheet.ready();
-    if (ready && millis() - lastTime > timerDelay) {
-        lastTime = millis();
-
-        temp = dht_sensor.readTemperature();
-        humi = dht_sensor.readHumidity();
-        pres = random(0, 100);
-        altitude = random(0, 100);
-        longitude = random(100, 5000);
-        epochTime = getTime();
-
-        appendToGSheet(temp, humi, pres, altitude, longitude, epochTime);
     }
 }
 
@@ -86,6 +83,8 @@ void File_handle()
 
 void WiFi_setup() {
 
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.print("Connecting to Wi-Fi");
 
     while (WiFi.status() != WL_CONNECTED) {
         Serial.print(".");
@@ -99,250 +98,106 @@ void WiFi_setup() {
     digitalWrite(WifiConnectedLed, HIGH);
 }
 
+void firebase_setup() {
+    config.api_key = API_KEY;
+    config.database_url = DATABASE_URL;
+    if (Firebase.signUp(&config, &auth, "", "")) {
+        Serial.println("Firebase signup successful");
+        signupOK = true;
+    } else {
+        Serial.printf("%s\n", config.signer.signupError.message.c_str());
+    }
+    config.token_status_callback = tokenStatusCallback;
+    Firebase.begin(&config, &auth);
+    Firebase.reconnectWiFi(true);
 
-// #include "main.h"
+}
 
-// // Timer variables
-// unsigned long lastTime = 0;
-// unsigned long timerDelay = 10000;
-// unsigned long sendDataPrevMillis = 0;
-// bool signupOK = false;
-// int count = 0;
+void temperature_humidity_firebase(float Temperature, float Humidity) {
+    if (Firebase.RTDB.setFloat(&fbdo, "dht/humidity", Humidity)) {
+        Serial.print("\nHumidity: ");
+        Serial.println(Humidity);
+        Serial.println("\nPASSED to Firebase");
+        Serial.print("\nPATH: ");
+        Serial.print(fbdo.dataPath());
+        Serial.print("  TYPE: ");
+        Serial.print(fbdo.dataType());
+    } else {
+        Serial.println("\nFAILED");
+        Serial.print("\nREASON: ");
+        Serial.print(fbdo.errorReason());
+    }
 
-// // Firebase objects
-// FirebaseData fbdo;
-// FirebaseAuth auth;
-// FirebaseConfig config;
-// WebServer server(80);
+    if (Firebase.RTDB.setInt(&fbdo, "dht/temperature", Temperature)) {
+        Serial.print("\nTemperature: ");
+        Serial.println(Temperature);
+        Serial.println("\nPASSED to Firebase");
+        Serial.print("\nPATH: ");
+        Serial.print(fbdo.dataPath());
+        Serial.print("\nTYPE: ");
+        Serial.print(fbdo.dataType());
+    } else {
+        Serial.println("\nFAILED");
+        Serial.print("\nREASON: ");
+        Serial.print(fbdo.errorReason());
+    }
+}
 
-// // DHT sensor
-// DHT dht_sensor(Dht_Sensor_Pin, Dht_type);
+void current_voltage_capacity_to_firebase() {
 
-// void setup()
-// {
-//   Serial.begin(115200);
-//   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
-//   Serial.print("Connecting to Wi-Fi");
-//   // pinMode(WifiConnectedLed, OUTPUT);
-//   //  setup_googlesheet();
+    if (Firebase.ready()) {
+        if (Firebase.RTDB.setInt(&fbdo, "capacity", capacity)) {
+            Serial.print("\nCapacity: ");
+            Serial.println(capacity);
+            Serial.println("\nPASSED to Firebase");
+        } else {
+            Serial.println("\nFAILED");
+            Serial.print("\nREASON: ");
+            Serial.print(fbdo.errorReason());
+        }
 
-//   if (!LittleFS.begin(true))
-//   {
-//     Serial.print("error occure while littleFs monuting ");
-//     return;
-//   }
+        if (Firebase.RTDB.setInt(&fbdo, "current", current)) {
+            Serial.print("\nCurrent: ");
+            Serial.println(current);
+            Serial.println("\nPASSED to Firebase");
+        } else {
+            Serial.println("\nFAILED");
+            Serial.print("\nREASON: ");
+            Serial.print(fbdo.errorReason());
+        }
 
-//   File file = LittleFS.open("/data.txt");
+        if (Firebase.RTDB.setInt(&fbdo, "voltage", voltage)) {
+            Serial.print("\nVoltage: ");
+            Serial.println(voltage);
+            Serial.println("\nPASSED to Firebase");
+        } else {
+            Serial.println("\nFAILED");
+            Serial.print("\nREASON: ");
+            Serial.print(fbdo.errorReason());
+        }
 
-//   if (!file)
-//   {
-//     Serial.print("can't open the file");
-//   }
+        if (Firebase.RTDB.setInt(&fbdo, "kmrange", Kmrange)) {
+            Serial.print("\nKmrange: ");
+            Serial.println(Kmrange);
+            Serial.println("\nPASSED to Firebase");
+        } else {
+            Serial.println("\nFAILED");
+            Serial.print("\nREASON: ");
+            Serial.print(fbdo.errorReason());
+        }
+    }
+}
 
-//   Serial.print("file content are:");
-//   while (file.available())
-//   {
-//     Serial.write(file.read());
-//   }
-
-//   Serial.print("readed and closed");
-//   file.close();
-
-//   WiFi_setup();
-
-//   /* Assign the api key (required) */
-//   config.api_key = API_KEY;
-
-//   /* Assign the RTDB URL (required) */
-//   config.database_url = DATABASE_URL;
-
-//   /* Sign up */
-//   if (Firebase.signUp(&config, &auth, "", ""))
-//   {
-//     Serial.println("ok");
-//     signupOK = true;
-//   }
-//   else
-//   {
-//     Serial.printf("%s\n", config.signer.signupError.message.c_str());
-//   }
-
-//   /* Assign the callback function for the long running token generation task */
-//   config.token_status_callback = tokenStatusCallback; // see addons/TokenHelper.h
-
-//   Firebase.begin(&config, &auth);
-//   //Firebase.reconnectWiFi(true);
-// }
-
-
-// void WiFi_setup()
-// {
-//  while (WiFi.status() != WL_CONNECTED)
-//   {
-//     Serial.print(".");
-//     delay(500);
-//   }
-//   Serial.println();
-
-//   Serial.print("Connected with IP: ");
-//   Serial.println(WiFi.localIP());
-//   Serial.println();
-//   digitalWrite(WifiConnectedLed, HIGH);
-
-// }
-
-// void temperature_humidity_firebase(float Temperature, float Humidity)
-// {
-//   // Write an Float number on the database path test/float
-//   if (Firebase.RTDB.setFloat(&fbdo, "dht/humidity", Humidity))
-//   {
-
-//     Serial.print("\nHumidity:  ");
-//     Serial.println(Humidity);
-//     Serial.println("\nPASSED to firebase");
-//     Serial.print("\nPATH: ");
-//     Serial.print(fbdo.dataPath());
-//     Serial.print("  TYPE: ");
-//     Serial.print(fbdo.dataType());
-//   }
-//   else
-//   {
-//     Serial.println("\nFAILED");
-//     Serial.print("\nREASON: ");
-//     Serial.print(fbdo.errorReason());
-//   }
-
-//   // Write an Int number on the database path test/int
-//   if (Firebase.RTDB.setInt(&fbdo, "dht/temperature", Temperature))
-//   {
-
-//     Serial.print("\nTemperature:  ");
-//     Serial.println(Temperature);
-//     Serial.println("\nPASSED to firebase");
-//     Serial.print("\nPATH: ");
-//     Serial.print(fbdo.dataPath());
-
-//     Serial.print("\nTYPE:  ");
-//     Serial.print(fbdo.dataType());
-//   }
-//   else
-//   {
-//     Serial.println("\nFAILED");
-//     Serial.print("REASON: ");
-//     Serial.print(fbdo.errorReason());
-//   }
-// }
-
-// void current_voltage_capacity_to_firebase()
-// {
-//   if (Firebase.RTDB.setFloat(&fbdo, "battery/capacity", capacity))
-//   {
-
-//     Serial.print("\nCapacity:  ");
-//     Serial.println(capacity);
-//     Serial.println("\nPASSED to firebase");
-//     Serial.print("\nPATH: ");
-//     Serial.print(fbdo.dataPath());
-//     Serial.print("  TYPE: ");
-//     Serial.print(fbdo.dataType());
-//   }
-//   else
-//   {
-//     Serial.println("\nFAILED");
-//     Serial.print("\nREASON: ");
-//     Serial.print(fbdo.errorReason());
-//   }
-
-//   if (Firebase.RTDB.setFloat(&fbdo, "battery/current", current))
-//   {
-
-//     Serial.print("\nRnadom Current:  ");
-//     Serial.println(current);
-//     Serial.println("\nPASSED to firebase");
-//     Serial.print("\nPATH: ");
-//     Serial.print(fbdo.dataPath());
-//     Serial.print("  TYPE: ");
-//     Serial.print(fbdo.dataType());
-//   }
-//   else
-//   {
-//     Serial.println("\nFAILED");
-//     Serial.print("\nREASON: ");
-//     Serial.print(fbdo.errorReason());
-//   }
-
-//   if (Firebase.RTDB.setFloat(&fbdo, "battery/voltage", voltage))
-//   {
-
-//     Serial.print("\nRandom Voltage:  ");
-//     Serial.println(current);
-//     Serial.println("\nPASSED to firebase");
-//     Serial.print("\nPATH: ");
-//     Serial.print(fbdo.dataPath());
-//     Serial.print(" TYPE: ");
-//     Serial.print(fbdo.dataType());
-//   }
-//   else
-//   {
-//     Serial.println("\nFAILED");
-//     Serial.print("\nREASON: ");
-//     Serial.print(fbdo.errorReason());
-//   }
-
-//   if (Firebase.RTDB.setFloat(&fbdo, "battery/kmrange", Kmrange))
-//   {
-
-//     Serial.print("\nRandom Range:  ");
-//     Serial.println(current);
-//     Serial.println("\nPASSED to firebase");
-//     Serial.print("\nPATH: ");
-//     Serial.print(fbdo.dataPath());
-//     Serial.print(" TYPE: ");
-//     Serial.print(fbdo.dataType());
-//   }
-//   else
-//   {
-//     Serial.println("\nFAILED");
-//     Serial.print("\nREASON: ");
-//     Serial.print(fbdo.errorReason());
-//   }
-// }
-
-// void check_button()
-// {
-//     if (!digitalRead(switch_pin))
-//   {
-//     led_status = !led_status;
-//     Serial.print("button is pressed to change led status ");
-//     Serial.print(led_status);
-//   }
-// }
-
-// void loop()
-// {
-
-//   check_button();
-//   // loop_googlesheet();
-
-//   if (Firebase.ready() && signupOK && (millis() - sendDataPrevMillis > 5000 || sendDataPrevMillis == 0))
-//   {
-//     sendDataPrevMillis = millis();
-
-//     Humi = dht_sensor.readTemperature();
-//     Temp = dht_sensor.readHumidity();
-//     digitalWrite(DataSendedLed, HIGH);
-
-//     temperature_humidity_firebase(Temp, Humi);
-//     current_voltage_capacity_to_firebase();
-
-//     if (Firebase.RTDB.getBool(&fbdo, "switch/led_status"))
-//     {
-//       led_status = fbdo.boolData();
-//       digitalWrite(database_led, led_status);
-//     }
-//     else
-//     {
-//       Serial.print("switch is not getted");
-//     }
-//   }
-// }
+void check_button_status() {
+    if (Firebase.ready()) {
+        if (Firebase.RTDB.setInt(&fbdo, "led", led_status)) {
+            Serial.print("\nLED Status: ");
+            Serial.println(led_status);
+            Serial.println("\nPASSED to Firebase");
+        } else {
+            Serial.println("\nFAILED");
+            Serial.print("\nREASON: ");
+            Serial.print(fbdo.errorReason());
+        }
+    }
+}
